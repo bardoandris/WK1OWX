3cf2ccede00e02800fb15bf99e98b1cd3393682f
Andras Bardonicsek <bardoandris07@gmail.com>
2022-10-14 14:37:10 +0200

    Local Change

--- WK1OWX/AboutForm.cs
+++ WK1OWX/AboutForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,23 +13,34 @@ namespace WK1OWX
 {
 	public partial class AboutForm : Form
 	{
+		CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+		CancellationToken CancellationToken;
 		public AboutForm()
 		{
 			InitializeComponent();
 		}
 
-		private async void AboutForm_Load(object sender, EventArgs e)
+		private void AboutForm_Load(object sender, EventArgs e)
 		{
-			while (true)
-			{
-				TimeLabel.Text = "Jelenlegi idő: " + DateTime.Now; 
-				await Task.Delay(1000);
-			}
+			CancellationToken = cancellationTokenSource.Token;
+			UpdateClock(CancellationToken);
 		}
 
 		private void CloseButton_Click(object sender, EventArgs e)
 		{
+			cancellationTokenSource.Cancel();
 			this.Close();
+		}
+		private async void UpdateClock(CancellationToken token)
+		{
+			while (!token.IsCancellationRequested)
+			{
+			//	try
+			//	{
+					TimeLabel.Invoke(() => TimeLabel.Text = "Jelenlegi idő: " + DateTime.Now);
+					await Task.Delay(1000);
+				//}catch(Exception e) { break; }
+			}
 		}
 	}
 }
